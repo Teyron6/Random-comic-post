7b6860e50acd14f24e274fe5df3ccbd026c83f43
Teyron6 <glushakov.tema227@mail.ru>
2024-10-05 14:48:32 +0500

    Исправил несколько ошибок

--- main.py
+++ main.py
@@ -8,41 +8,44 @@ import random
 def get_amount_of_comics():
     url = 'https://xkcd.com/info.0.json'
     response = requests.get(url)
-    response_json = response.json()
-    comics_amount = response_json['num']
+    response.raise_for_status()
+    response = response.json()
+    comics_amount = response['num']
     return comics_amount
 
 
 def get_comic(comic_id):
     url = f'https://xkcd.com/{comic_id}/info.0.json'
     response = requests.get(url)
-    response_json = response.json()
-    comic_img_url = response_json['img']
-    comic_img = requests.get(comic_img_url)
     response.raise_for_status()
-    with open(f'comics/{comic_id}.png', 'wb') as f:
+    response = response.json()
+    comic_img_url = response['img']
+    comic_name = response['title']
+    comment = response['alt']
+    comic_img = requests.get(comic_img_url)
+    with open(f'{comic_name}.png', 'wb') as f:
         f.write(comic_img.content)
-    return {'caption' : response_json['alt'],
-            'image_id' : comic_id,
-            }
+    return comic_name, comment
 
 
-def post_random_comic(token, comics_amount, chat_id):
+def post_random_comic(token, image_name, chat_id, comment):
     bot = telegram.Bot(token=token)
-    comic_info = get_comic(random.randint(1, comics_amount))
-    image_id = comic_info['image_id']
-    with open(f'comics/{image_id}.png', 'rb') as f: 
-        bot.send_photo(chat_id=chat_id, photo=f, caption=comic_info['caption'])
-    os.remove(f'comics/{image_id}.png')
+    with open(f'{image_name}.png', 'rb') as f: 
+        bot.send_photo(chat_id=chat_id, photo=f, caption=comment)
 
 
 def main():
     load_dotenv()
-    token=os.environ.get('TG_TOKEN')
-    comics_amount = get_amount_of_comics()
-    chat_id = os.environ.get('GROUP_ID')
-    post_random_comic(token, comics_amount, chat_id)
-    
+    token = os.environ['TG_TOKEN']
+    chat_id = os.environ['GROUP_ID']
+    try:
+        comics_amount = get_amount_of_comics()
+        comic_info = random.randint(1, comics_amount)
+        image_name, comment = get_comic(comic_info)
+        post_random_comic(token, image_name, chat_id, comment)
+    finally:
+        os.remove(f'{image_name}.png')
+
 
 if __name__ == '__main__':
     main()
